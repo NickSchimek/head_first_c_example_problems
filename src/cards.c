94beb9ab227bbaf71f60a3df95b44f689b5373bf
Nick Schimek <cfme00@gmail.com>
2018-09-03 21:24:11 -0700

    Add control loop

--- src/cards.c
+++ src/cards.c
@@ -9,31 +9,43 @@
 int main()
 {
     char card_name[3];
-    puts("Enter the card_name: ");
-    scanf("%2s", card_name);
-    int val = 0;
-    switch card_name[0]
-    {
-    case 'K':
-    case 'Q':
-    case 'J':
-        val = 10;
-        break;
-    case 'A':
-        val = 11;
-        break;
-    default:
-        val = atoi(card_name);
-    }
+    int count = 0;
+    while (card_name[0] != 'X') {
+        puts("Enter the card_name: ");
+        scanf("%2s", card_name);
+        int val = 0;
+        switch (card_name[0])
+        {
+        case 'K':
+        case 'Q':
+        case 'J':
+            val = 10;
+            break;
+        case 'A':
+            val = 11;
+            break;
+        case 'X':
+            continue;
+        default:
+            val = atoi(card_name);
+            if (val < 2 || val > 10) {
+                puts("I don't understand that value!");
+                continue;
+            }
+        }
 
-    /* Check if the value is 3 to 6 */
-    if ((val > 2) && (val < 7))
-    {
-        puts("Count has gone up");
-    }
-    else if (val == 10)
-    {
-        puts("Count has gone down");
+        /* Check if the value is 3 to 6 */
+        if ((val > 2) && (val < 7))
+        {
+            count++;
+            puts("Count has gone up");
+        }
+        else if (val == 10)
+        {
+            count--;
+            puts("Count has gone down");
+        }
+        printf("Current count: %i\n", count);
     }
     return 0;
 }
